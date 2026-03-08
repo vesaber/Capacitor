fae6934b000b609064b4f8e355ddef781834e123
vesaber <vesaber18@gmail.com>
2026-03-08 22:02:52 +0100

    fix: deleted update and whitelist

--- commands/index.ts
+++ commands/index.ts
@@ -13,10 +13,8 @@ import balance from "./balance";
 import daily from "./daily";
 import give from "./give";
 import coinflip from "./coinflip";
-import whitelist from "./whitelist";
-import update from "./update";
 import eightball from "./8ball";
 import coin from "./coin";
 import roll from "./roll";
 
-export { about, kick, ban, unban, timeout, untimeout, warn, unwarn, warnings, rank, leaderboard, balance, daily, give, coinflip, whitelist, update, eightball, coin, roll };
+export { about, kick, ban, unban, timeout, untimeout, warn, unwarn, warnings, rank, leaderboard, balance, daily, give, coinflip, eightball, coin, roll };
